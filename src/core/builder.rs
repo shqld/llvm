3eb548bf617136feadefc5310db2a1fcb30b1d69
Sho Miyamoto <me@shqld.dev>
2023-10-26 01:15:56 +0900

    add misc builder methods

--- src/core/builder.rs
+++ src/core/builder.rs
@@ -1,8 +1,11 @@
 use std::{cell::Cell, marker::PhantomData};
 
-use llvm_sys::{core::*, LLVMBuilder, LLVMOpcode};
+use llvm_sys::{core::*, LLVMAtomicOrdering, LLVMAtomicRMWBinOp, LLVMBuilder, LLVMOpcode};
 
-use crate::{ffi::nullstr, BasicBlock, Instruction, LLVMRef, Metadata, Type, Value};
+use crate::{
+	ffi::nullstr, types::FunctionType, BasicBlock, Function, Instruction, LLVMRef, Metadata, Type,
+	Value,
+};
 
 #[derive(Debug, PartialEq, Eq)]
 pub struct Builder<'ctx, 'mdl, 'fnc, 'blk> {
@@ -680,4 +683,398 @@ impl<'ctx> Builder<'ctx, '_, '_, '_> {
 	//   Str: *const ::libc::c_char,
 	//   Name: *const ::libc::c_char,
 	// ) -> LLVMValueRef;
+}
+
+impl<'ctx, 'mdl> Builder<'ctx, 'mdl, '_, '_> {
+	// pub fn LLVMBuildPhi(
+	//     arg1: LLVMBuilderRef,
+	//     Ty: LLVMTypeRef,
+	//     Name: *const ::libc::c_char,
+	// ) -> LLVMValueRef;
+	pub fn build_phi(&self, ty: &Type<'ctx>) -> Instruction {
+		Instruction::new(unsafe { LLVMBuildPhi(self.as_mut_ptr(), ty.as_mut_ptr(), nullstr) })
+	}
+
+	// pub fn LLVMBuildCall2(
+	//     arg1: LLVMBuilderRef,
+	//     arg2: LLVMTypeRef,
+	//     Fn: LLVMValueRef,
+	//     Args: *mut LLVMValueRef,
+	//     NumArgs: ::libc::c_uint,
+	//     Name: *const ::libc::c_char,
+	// ) -> LLVMValueRef;
+	pub fn build_call(
+		&self,
+		ty: FunctionType<'ctx>,
+		func: &Function<'ctx, 'mdl>,
+		args: &[&Value],
+	) -> Result<Instruction, String> {
+		let mut args = args
+			.iter()
+			.map(|v| unsafe { v.as_mut_ptr() })
+			.collect::<Vec<_>>();
+
+		Ok(Instruction::new(unsafe {
+			LLVMBuildCall2(
+				self.as_mut_ptr(),
+				// func.ty() is opaque pointer type, so we have to get the type from args
+				ty.as_mut_ptr(),
+				func.as_mut_ptr(),
+				args.as_mut_ptr(),
+				args.len() as u32,
+				nullstr,
+			)
+		}))
+	}
+
+	// pub fn LLVMBuildSelect(
+	//     arg1: LLVMBuilderRef,
+	//     If: LLVMValueRef,
+	//     Then: LLVMValueRef,
+	//     Else: LLVMValueRef,
+	//     Name: *const ::libc::c_char,
+	// ) -> LLVMValueRef;
+	pub fn build_select(
+		&self,
+		cond: &Value,
+		then: &Value,
+		els: &Value,
+	) -> Result<Instruction, String> {
+		if !cond.ty().is_int_ty() {
+			return Err(format!(
+				"Cannot select value of non-integer type '{}'",
+				cond.ty()
+			));
+		}
+
+		if !then.ty().is_ptr_ty() || !els.ty().is_ptr_ty() {
+			return Err(format!(
+				"Cannot select value of non-pointer type '{}'",
+				then.ty()
+			));
+		}
+
+		Ok(Instruction::new(unsafe {
+			LLVMBuildSelect(
+				self.as_mut_ptr(),
+				cond.as_mut_ptr(),
+				then.as_mut_ptr(),
+				els.as_mut_ptr(),
+				nullstr,
+			)
+		}))
+	}
+
+	// pub fn LLVMBuildVAArg(
+	//     arg1: LLVMBuilderRef,
+	//     List: LLVMValueRef,
+	//     Ty: LLVMTypeRef,
+	//     Name: *const ::libc::c_char,
+	// ) -> LLVMValueRef;
+	pub fn build_vararg(&self, list: &Value, ty: &Type<'ctx>) -> Instruction {
+		Instruction::new(unsafe {
+			LLVMBuildVAArg(
+				self.as_mut_ptr(),
+				list.as_mut_ptr(),
+				ty.as_mut_ptr(),
+				nullstr,
+			)
+		})
+	}
+
+	// pub fn LLVMBuildExtractElement(
+	//     arg1: LLVMBuilderRef,
+	//     VecVal: LLVMValueRef,
+	//     Index: LLVMValueRef,
+	//     Name: *const ::libc::c_char,
+	// ) -> LLVMValueRef;
+	pub fn build_extract_element(&self, vec: &Value, index: &Value) -> Result<Instruction, String> {
+		if !vec.ty().is_vector_ty() || !vec.ty().is_array_ty() {
+			return Err(format!(
+				"Cannot extract element from non-vector type '{}'",
+				vec.ty()
+			));
+		}
+
+		if !index.ty().is_int_ty() {
+			return Err(format!(
+				"Cannot extract element with non-integer index '{}'",
+				index.ty()
+			));
+		}
+
+		Ok(Instruction::new(unsafe {
+			LLVMBuildExtractElement(
+				self.as_mut_ptr(),
+				vec.as_mut_ptr(),
+				index.as_mut_ptr(),
+				nullstr,
+			)
+		}))
+	}
+
+	// pub fn LLVMBuildInsertElement(
+	//     arg1: LLVMBuilderRef,
+	//     VecVal: LLVMValueRef,
+	//     EltVal: LLVMValueRef,
+	//     Index: LLVMValueRef,
+	//     Name: *const ::libc::c_char,
+	// ) -> LLVMValueRef;
+	pub fn build_insert_element(
+		&self,
+		vec: &Value,
+		elt: &Value,
+		index: &Value,
+	) -> Result<Instruction, String> {
+		if !vec.ty().is_vector_ty() || !vec.ty().is_array_ty() {
+			return Err(format!(
+				"Cannot insert element into non-vector type '{}'",
+				vec.ty()
+			));
+		}
+
+		if !index.ty().is_int_ty() {
+			return Err(format!(
+				"Cannot insert element with non-integer index '{}'",
+				index.ty()
+			));
+		}
+
+		Ok(Instruction::new(unsafe {
+			LLVMBuildInsertElement(
+				self.as_mut_ptr(),
+				vec.as_mut_ptr(),
+				elt.as_mut_ptr(),
+				index.as_mut_ptr(),
+				nullstr,
+			)
+		}))
+	}
+
+	// pub fn LLVMBuildShuffleVector(
+	//     arg1: LLVMBuilderRef,
+	//     V1: LLVMValueRef,
+	//     V2: LLVMValueRef,
+	//     Mask: LLVMValueRef,
+	//     Name: *const ::libc::c_char,
+	// ) -> LLVMValueRef;
+	pub fn build_shuffle_vector(
+		&self,
+		v1: &Value,
+		v2: &Value,
+		mask: &Value,
+	) -> Result<Instruction, String> {
+		if !v1.ty().is_vector_ty() || !v1.ty().is_array_ty() {
+			return Err(format!(
+				"Cannot shuffle vector of non-vector type '{}'",
+				v1.ty()
+			));
+		}
+
+		if !v2.ty().is_vector_ty() || !v2.ty().is_array_ty() {
+			return Err(format!(
+				"Cannot shuffle vector of non-vector type '{}'",
+				v2.ty()
+			));
+		}
+
+		if !mask.ty().is_vector_ty() || !mask.ty().is_array_ty() {
+			return Err(format!(
+				"Cannot shuffle vector with non-vector mask '{}'",
+				mask.ty()
+			));
+		}
+
+		Ok(Instruction::new(unsafe {
+			LLVMBuildShuffleVector(
+				self.as_mut_ptr(),
+				v1.as_mut_ptr(),
+				v2.as_mut_ptr(),
+				mask.as_mut_ptr(),
+				nullstr,
+			)
+		}))
+	}
+
+	// pub fn LLVMBuildExtractValue(
+	//     arg1: LLVMBuilderRef,
+	//     AggVal: LLVMValueRef,
+	//     Index: ::libc::c_uint,
+	//     Name: *const ::libc::c_char,
+	// ) -> LLVMValueRef;
+	pub fn build_extract_value(&self, agg: &Value, index: u32) -> Result<Instruction, String> {
+		if !agg.ty().is_struct_ty() {
+			return Err(format!(
+				"Cannot extract value from non-struct type '{}'",
+				agg.ty()
+			));
+		}
+
+		Ok(Instruction::new(unsafe {
+			LLVMBuildExtractValue(self.as_mut_ptr(), agg.as_mut_ptr(), index, nullstr)
+		}))
+	}
+
+	// pub fn LLVMBuildInsertValue(
+	//     arg1: LLVMBuilderRef,
+	//     AggVal: LLVMValueRef,
+	//     EltVal: LLVMValueRef,
+	//     Index: ::libc::c_uint,
+	//     Name: *const ::libc::c_char,
+	// ) -> LLVMValueRef;
+	pub fn build_insert_value(
+		&self,
+		agg: &Value,
+		elt: &Value,
+		index: u32,
+	) -> Result<Instruction, String> {
+		if !agg.ty().is_struct_ty() {
+			return Err(format!(
+				"Cannot insert value into non-struct type '{}'",
+				agg.ty()
+			));
+		}
+
+		Ok(Instruction::new(unsafe {
+			LLVMBuildInsertValue(
+				self.as_mut_ptr(),
+				agg.as_mut_ptr(),
+				elt.as_mut_ptr(),
+				index,
+				nullstr,
+			)
+		}))
+	}
+
+	// pub fn LLVMBuildFreeze(
+	//     arg1: LLVMBuilderRef,
+	//     Val: LLVMValueRef,
+	//     Name: *const ::libc::c_char,
+	// ) -> LLVMValueRef;
+	pub fn build_freeze(&self, val: &Value) -> Instruction {
+		Instruction::new(unsafe { LLVMBuildFreeze(self.as_mut_ptr(), val.as_mut_ptr(), nullstr) })
+	}
+
+	// pub fn LLVMBuildIsNull(
+	//     arg1: LLVMBuilderRef,
+	//     Val: LLVMValueRef,
+	//     Name: *const ::libc::c_char,
+	// ) -> LLVMValueRef;
+	pub fn build_is_null(&self, val: &Value) -> Instruction {
+		Instruction::new(unsafe { LLVMBuildIsNull(self.as_mut_ptr(), val.as_mut_ptr(), nullstr) })
+	}
+
+	// pub fn LLVMBuildIsNotNull(
+	//     arg1: LLVMBuilderRef,
+	//     Val: LLVMValueRef,
+	//     Name: *const ::libc::c_char,
+	// ) -> LLVMValueRef;
+	pub fn build_is_not_null(&self, val: &Value) -> Instruction {
+		Instruction::new(unsafe {
+			LLVMBuildIsNotNull(self.as_mut_ptr(), val.as_mut_ptr(), nullstr)
+		})
+	}
+
+	// pub fn LLVMBuildPtrDiff2(
+	//     arg1: LLVMBuilderRef,
+	//     ElemTy: LLVMTypeRef,
+	//     LHS: LLVMValueRef,
+	//     RHS: LLVMValueRef,
+	//     Name: *const ::libc::c_char,
+	// ) -> LLVMValueRef;
+	pub fn build_ptr_diff(
+		&self,
+		elem_ty: &Type<'ctx>,
+		lhs: &Value,
+		rhs: &Value,
+	) -> Result<Instruction, String> {
+		if !lhs.ty().is_ptr_ty() || !rhs.ty().is_ptr_ty() {
+			return Err(format!(
+				"Cannot compute pointer difference between non-pointer types '{}', '{}'",
+				lhs.ty(),
+				rhs.ty()
+			));
+		}
+
+		Ok(Instruction::new(unsafe {
+			LLVMBuildPtrDiff2(
+				self.as_mut_ptr(),
+				elem_ty.as_mut_ptr(),
+				lhs.as_mut_ptr(),
+				rhs.as_mut_ptr(),
+				nullstr,
+			)
+		}))
+	}
+
+	// pub fn LLVMBuildFence(
+	//     B: LLVMBuilderRef,
+	//     ordering: LLVMAtomicOrdering,
+	//     singleThread: LLVMBool,
+	//     Name: *const ::libc::c_char,
+	// ) -> LLVMValueRef;
+	pub fn build_fence(&self, ordering: LLVMAtomicOrdering, single_thread: bool) -> Instruction {
+		Instruction::new(unsafe {
+			LLVMBuildFence(self.as_mut_ptr(), ordering, single_thread.into(), nullstr)
+		})
+	}
+
+	// pub fn LLVMBuildAtomicRMW(
+	//     B: LLVMBuilderRef,
+	//     op: LLVMAtomicRMWBinOp,
+	//     PTR: LLVMValueRef,
+	//     Val: LLVMValueRef,
+	//     ordering: LLVMAtomicOrdering,
+	//     singleThread: LLVMBool,
+	// ) -> LLVMValueRef;
+	pub fn build_atomic_rmw(
+		&self,
+		op: LLVMAtomicRMWBinOp,
+		ptr: &Value,
+		val: &Value,
+		ordering: LLVMAtomicOrdering,
+		single_thread: bool,
+	) -> Instruction {
+		Instruction::new(unsafe {
+			LLVMBuildAtomicRMW(
+				self.as_mut_ptr(),
+				op,
+				ptr.as_mut_ptr(),
+				val.as_mut_ptr(),
+				ordering,
+				single_thread.into(),
+			)
+		})
+	}
+
+	// pub fn LLVMBuildAtomicCmpXchg(
+	//     B: LLVMBuilderRef,
+	//     Ptr: LLVMValueRef,
+	//     Cmp: LLVMValueRef,
+	//     New: LLVMValueRef,
+	//     SuccessOrdering: LLVMAtomicOrdering,
+	//     FailureOrdering: LLVMAtomicOrdering,
+	//     SingleThread: LLVMBool,
+	// ) -> LLVMValueRef;
+	pub fn build_atomic_cmp_xchg(
+		&self,
+		ptr: &Value,
+		cmp: &Value,
+		new: &Value,
+		success_ordering: LLVMAtomicOrdering,
+		failure_ordering: LLVMAtomicOrdering,
+		single_thread: bool,
+	) -> Instruction {
+		Instruction::new(unsafe {
+			LLVMBuildAtomicCmpXchg(
+				self.as_mut_ptr(),
+				ptr.as_mut_ptr(),
+				cmp.as_mut_ptr(),
+				new.as_mut_ptr(),
+				success_ordering,
+				failure_ordering,
+				single_thread.into(),
+			)
+		})
+	}
 }
